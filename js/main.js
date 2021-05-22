57bae9b0cf5a5753f69c526720b960b9d2d47934
SiraKen <shirasawa.kento@gmail.com>
2021-05-22 15:08:05 +0000

    Deploying to gh-pages from  @ 7fd7585b24391e58ec40ea454586b12df8ff415a 🚀

--- js/main.js
+++ js/main.js
@@ -1,49 +1,43 @@
 $(function() {
-
-    $('.btn-open').on('click', function() {
-      $('#overlay').fadeIn();
-    });
-    
-    $('.close-btn').on('click', function() {
-      $('#overlay').fadeOut();
-    });
   
-    $('#overlay').on('click', function() {
-      $('#overlay').fadeOut();
-      sp_menu.classList.remove('active');
-      body.style['overflow-y'] = 'auto';
-    });
-
-    $('.header-menu-item-sp > a').on('click', function() {
-      $('#overlay').fadeOut();
-      sp_menu.classList.remove('active');
-      body.style['overflow-y'] = 'auto';
-    });
-  
-  });
-  
-  let sp_menu = document.getElementById('sp_menu');
-  let body    = document.body;
-  
-  function openSmartMenu() {
-    sp_menu.classList.toggle('active');
-    body.style['overflow-y'] = 'hidden';
-  }
-  
-  function closeSmartMenu() {
+  $('#overlay').on('click', function() {
+    $('#overlay').fadeOut();
     sp_menu.classList.remove('active');
     body.style['overflow-y'] = 'auto';
-  }
-  
-  // Enter
-  let input = [];
-  code = [38,38,40,40,37,39,37,39,66,65];
-  
-  $(window).keyup(function(e){
-  input.push(e.keyCode);
-  
-  if (input.toString().indexOf(code) >= 0)
-    {
-      window.location.href = '/recruit/'; // Coming soon
-    }
   });
+
+  $('.header-menu-item-sp > a').on('click', function() {
+    $('#overlay').fadeOut();
+    sp_menu.classList.remove('active');
+    body.style['overflow-y'] = 'auto';
+  });
+  
+});
+  
+let sp_menu = document.getElementById('sp_menu');
+let body    = document.body;
+
+function openSmartMenu() {
+  $('#overlay').fadeIn();
+  sp_menu.classList.toggle('active');
+  body.style['overflow-y'] = 'hidden';
+}
+
+function closeSmartMenu() {
+  $('#overlay').fadeOut();
+  sp_menu.classList.remove('active');
+  body.style['overflow-y'] = 'auto';
+}
+
+// Enter
+let input = [];
+code = [38,38,40,40,37,39,37,39,66,65];
+
+$(window).keyup(function(e){
+input.push(e.keyCode);
+
+if (input.toString().indexOf(code) >= 0)
+  {
+    window.location.href = '/recruit/'; // Coming soon
+  }
+});
