1956ccf0bb6f4b3496e6f120e95762d1a0d10f37
SiraKen <shirasawa.kento@gmail.com>
2021-05-23 03:37:14 +0000

    Deploying to gh-pages from  @ 4713e9ce34a064ef077a06e7b176fe2b5422ed7f 🚀

---
+++ _nuxt/static/1621741029/es/contact/result/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621741029",layout:"default",error:a,serverRendered:true,routePath:"\u002Fes\u002Fcontact\u002Fresult",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{es:{}}}}}(null));
