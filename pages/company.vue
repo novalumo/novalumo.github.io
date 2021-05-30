cf149051b5fd51e3bb2f1c70f48f589cc1d55e39
Kento Shirasawa <shirasawa.kento@gmail.com>
2021-05-30 12:54:47 +0900

    会社概要ページの翻訳（ミッション本文以外）完了

--- pages/company.vue
+++ pages/company.vue
@@ -13,42 +13,40 @@
         <div class="col"><!-- col -->
 
           <!-- mission -->
-          <h2 class="heading">ミッション</h2>
+          <h2 class="heading">{{ $t('company.mission.title') }}</h2>
           <article>
             <p>
-              “Novalumo”（ノバルーモ） は、国際補助語であるエスペラントで「新しい（Nova）光（Lumo）」という意味があります。
-              この言葉には「世界中に光を与え、人々を笑顔にしたい」という想いが込められています。
-              情報通信技術やエンターテインメントを通じて、多くの人々に勇気や希望、そして笑顔を届けることが、私たちの使命です。
+              {{ $t('company.mission.body') }}
             </p>
           </article>
 
           <!-- about -->
-          <h2 class="heading">会社概要</h2>
+          <h2 class="heading">{{ $t('company.overview.title') }}</h2>
           <table>
             <tbody>
               <tr>
-                <th>会社名</th>
+                <th>{{ $t('company.overview.name') }}</th>
                 <td>
                   <span><ruby>Novalumo<rt>ノバルーモ</rt></ruby>合同会社</span><br>
-                  <span>（英文表記）</span><span>Novalumo Japan G.K.</span>
+                  <span>{{ $t('company.overview.name_eng') }}：</span><span>Novalumo Japan G.K.</span>
                 </td>
               </tr>
               <tr>
-                <th>代表社員</th>
-                <td>白澤 賢斗</td>
+                <th>{{ $t('company.overview.rep') }}</th>
+                <td>{{ $t('company.overview.rep_name') }}</td>
               </tr>
               <tr>
-                <th>設立</th>
-                <td><span>2020年</span><span>4月30日</span></td>
+                <th>{{ $t('company.overview.est') }}</th>
+                <td>{{ $t('company.overview.est_date') }}</td>
               </tr>
               <tr>
-                <th>資本金</th>
-                <td>30,000円</td>
+                <th>{{ $t('company.overview.capital') }}</th>
+                <td>{{ $t('company.overview.capital_price') }}</td>
               </tr>
               <tr>
-                <th><span>取引</span><span>銀行</span></th>
+                <th>{{ $t('company.overview.bank') }}</th>
                 <td>
-                  岩手銀行、東北銀行<br>
+                  {{ $t('company.overview.banks') }}<br>
                 </td>
               </tr>
             </tbody>
@@ -56,37 +54,22 @@
 
           <!-- officer -->
           <div>
-            <h2 class="heading">役員</h2>
+            <h2 class="heading">{{ $t('company.officer.title') }}</h2>
             <table>
               <tbody>
                 <tr>
                   <th>
                     <span title="最高経営責任者">CEO</span>
                   </th>
-                  <td>白澤 賢斗 <a href="https://siraken.github.io/" target="_blank" rel="noopener noreferrer"><i class="fas fa-fw fa-external-link-alt"></i></a></td>
+                  <td>{{ $t('company.officer.ceo') }} <a href="https://siraken.github.io/" target="_blank" rel="noopener noreferrer"><i class="fas fa-fw fa-external-link-alt"></i></a></td>
                 </tr>
                 <tr>
                   <th><span title="最高執行責任者">COO</span></th>
-                  <td>白澤 里美</td>
+                  <td>{{ $t('company.officer.coo_1') }}</td>
                 </tr>
               </tbody>
             </table>
           </div>
-
-          <!-- history -->
-          <h2 class="heading">沿革</h2>
-          <table>
-            <tbody>
-              <tr>
-                <th><span>2020年</span> <span>4月</span></th>
-                <td>岩手県大船渡市にNovalumo合同会社を設立（30日）</td>
-              </tr>
-              <tr>
-                <th><span>2020年</span> <span>5月</span></th>
-                <td>法人登記手続き完了、事業開始（8日）</td>
-              </tr>
-            </tbody>
-          </table>
 
         </div><!-- /col -->
       </div><!-- /row -->
