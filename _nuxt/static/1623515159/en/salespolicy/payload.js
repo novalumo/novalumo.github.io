1d3c5b9bd4ed7b369a8afab2acd48a4618fcc5a2
SiraKen <shirasawa.kento@gmail.com>
2021-06-12 16:26:05 +0000

    Deploying to gh-pages from  @ 3928dbb0ebf23864a4246d48bfc0e87aa104308b 🚀

---
+++ _nuxt/static/1623515159/en/salespolicy/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/en/salespolicy", {data:[{}],fetch:{"data-v-92d706a8:0":{info:{company:"Novalumo合同会社",staff:"代表社員　白澤 賢斗",zipcode:"022-0002",address:"岩手県大船渡市大船渡町字山馬越64-3 県営住宅336号",tel:"080-2833-2098",email:"contact@novalumo.llc"},lang:"en"}},mutations:void 0});
