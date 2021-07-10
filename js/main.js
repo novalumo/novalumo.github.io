d6f2a8a456b711247d8b7fd05e17e1766264b690
SiraKen <shirasawa.kento@gmail.com>
2021-07-10 06:42:45 +0000

    Deploying to gh-pages from  @ 91ab881ed7456a16996b7d73fd22ac6c0615085d 🚀

--- js/main.js
+++ js/main.js
@@ -1,43 +1,22 @@
 $(function() {
-  
-  $('#overlay').on('click', function() {
-    $('#overlay').fadeOut();
-    sp_menu.classList.remove('active');
-    body.style['overflow-y'] = 'auto';
-  });
 
   $('.header-menu-item-sp > a').on('click', function() {
-    $('#overlay').fadeOut();
-    sp_menu.classList.remove('active');
-    body.style['overflow-y'] = 'auto';
-  });
+    $('#overlay').fadeOut()
+    document.getElementById('sp_menu').classList.remove('active')
+    document.body.style['overflow-y'] = 'auto'
+  })
   
-});
-  
-let sp_menu = document.getElementById('sp_menu');
-let body    = document.body;
-
-function openSmartMenu() {
-  $('#overlay').fadeIn();
-  sp_menu.classList.toggle('active');
-  body.style['overflow-y'] = 'hidden';
-}
-
-function closeSmartMenu() {
-  $('#overlay').fadeOut();
-  sp_menu.classList.remove('active');
-  body.style['overflow-y'] = 'auto';
-}
+})
 
 // Enter
-let input = [];
-code = [38,38,40,40,37,39,37,39,66,65];
+let input = []
+code = [38,38,40,40,37,39,37,39,66,65]
 
 $(window).keyup(function(e){
-input.push(e.keyCode);
+input.push(e.keyCode)
 
 if (input.toString().indexOf(code) >= 0)
   {
-    window.location.href = '/recruit/'; // Coming soon
+    window.location.href = '/recruit/' // Coming soon
   }
-});
+})
